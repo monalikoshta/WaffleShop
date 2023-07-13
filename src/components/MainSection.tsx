4ffdbd9e3a12a8898a89e45081f1703db444f44d
Monali Koshta <monalikoshta@Monalis-MacBook-Air.local>
2023-07-13 11:53:19 +0530

    landing image change

--- src/components/MainSection.tsx
+++ src/components/MainSection.tsx
@@ -3,7 +3,7 @@
 export const MainSection = () => {
   return (
     <div>
-      <div className="bg-[url(/assets/emma-houghton-yXs3kr1VdfY-unsplash.jpg)] bg-no-repeat bg-cover w-full bg-center h-[93vh] relative">
+      <div className="bg-[url(/assets/emma-houghton-yXs3kr1VdfY-unsplash.jpg)] bg-no-repeat bg-cover w-full bg-center h-[400px] md:h-[93vh] relative">
         {/* <NavBar /> */}
         <div
           className="text-white absolute bottom-40 w-full font-Diphylleia font-bold text-2xl md:text-5xl lg:text-7xl bg-[#b4b4ba5c] p-5 backdrop-blur-[2px] text-center"
